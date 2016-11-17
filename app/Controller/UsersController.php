5b074dca00e932274b526d31062382e71ee01b5d
ragrog <ragrog216@outlook.jp>
2016-11-17 15:41:31 +0900

    [add] UserページのMVCを作成
    ページはadd, login, logoutのみ

---
+++ app/Controller/UsersController.php
@@ -0,0 +1,22 @@
+<?php
+
+App::uses('AppController', 'Controller');
+
+class UsersController extends AppController {
+	public $uses = array('Users');
+
+	// ユーザ新規登録
+	public function add() {
+
+	}
+
+	// ユーザログイン
+	public function  login() {
+
+	}
+
+	// ユーザログアウト
+	public function logout() {
+
+	}
+}
